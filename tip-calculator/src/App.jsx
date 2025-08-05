1c69bd6f703ed8ddbb23209af3d65fd88af927c4
Christian Yarwood <christianyarwood@gmail.com>
2025-08-05 11:07:51 +0100

    Make app with Blank components

---
+++ tip-calculator/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import BillInput from './BillInput'
+import './App.css'
+
+function App() {
+
+  return (
+      <BillInput/>
+  )
+}
+
+export default App
---
+++ tip-calculator/src/BillInput.jsx
@@ -0,0 +1,12 @@
+export default function BillInput() {
+    return (
+        <div>
+            <span>
+                How much was the bill?
+            </span>
+            <input type="number">
+                
+            </input>
+        </div>
+    )
+}
