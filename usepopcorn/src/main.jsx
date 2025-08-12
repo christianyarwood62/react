b9211d4815dc05e0d63429033538d014d56da2b3
Christian Yarwood <christianyarwood@gmail.com>
2025-08-12 11:40:36 +0100

    Add rating message below stars using state

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+import StarRating from "./StarRating";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Awful", "Bad", "Decent", "Great", "The best"]}
+      defaultRating={3}
+    />
+    <Test></Test>
+  </StrictMode>
+);
