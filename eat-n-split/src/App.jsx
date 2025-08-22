a04f869d750c2341ec5ab57f55fa9f1a72d8234e
Christian Yarwood <christianyarwood@gmail.com>
2025-08-22 10:29:58 +0100

    Add conditional rendering to show addFriendForm

--- eat-n-split/src/App.jsx
+++ eat-n-split/src/App.jsx
@@ -21,13 +21,29 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "close" : "Add friend"}
+        </Button>
       </div>
 
       <FormSplitBill />
@@ -70,9 +86,6 @@ function Friend({ friend }) {
   );
 }
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
-}
 function FormAddFriend() {
   return (
     <form className="form-add-friend">
