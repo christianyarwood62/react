6dcd0221776d731d7579ac88a5de3646a216ef70
Christian Yarwood <christianyarwood@gmail.com>
2025-09-01 11:14:28 +0100

    remove test code

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -3,19 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import StarRating from "./StarRating";
 import { useState } from "react";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
