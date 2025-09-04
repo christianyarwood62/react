f43ab6ec7a1dc5134c9d0e1a09eea01a09a026d0
Christian Yarwood <christianyarwood@gmail.com>
2025-09-04 10:34:01 +0100

    Add escape key event listener

--- usepopcorn/src/App.jsx
+++ usepopcorn/src/App.jsx
@@ -379,12 +379,19 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   // If you put this useEffect in the App component then it would always listen for Escape even when no MovieDetails are showing on the right
   useEffect(
     function () {
-      document.addEventListener("keydown", function (e) {
+      function callback(e) {
+        // This function is placed here and referenced below to ensure the event listener gets removed
         if (e.code === "Escape") {
           onCloseMovie();
-          console.log("CLOSING");
         }
-      }); // Can use this addEventListener (A DOM function) because this steps out of react and can use plain js
+      }
+
+      // This is how to handle key events in react, you use useEffect and some plain js
+      document.addEventListener("keydown", callback); // Can use this addEventListener (A DOM function) because this steps out of react and can use plain js
+
+      return function () {
+        document.removeEventListener("keydown", callback); // Have to add this remove line because everytime the page renders, it would add an additional event listener, causing memory issues
+      };
     },
     [onCloseMovie] // Have to use this function in the dependency array
   );
@@ -413,6 +420,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       // This is the cleanup function
       return function () {
         document.title = "usePopcorn";
+        // console.log(`Clean up effect for movie {title}`);
       };
     },
     [title]
