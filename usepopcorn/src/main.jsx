ca1cae75f057594c842ad2e8b77474f438e5ae05
Christian Yarwood <christianyarwood@gmail.com>
2025-09-08 11:22:46 +0100

    remove unused useState import

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -3,16 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-import { useState } from "react";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    {/* <StarRating
-      maxRating={5}
-      messages={["Awful", "Bad", "Decent", "Great", "The best"]}
-      defaultRating={3}
-    /> */}
-    {/* <Test></Test> */}
   </StrictMode>
 );
