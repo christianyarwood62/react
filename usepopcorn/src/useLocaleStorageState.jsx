665957510894e49785be888d012940ea819eb1f4
Christian Yarwood <christianyarwood@gmail.com>
2025-09-09 09:14:04 +0100

    Add comments

--- usepopcorn/src/useLocaleStorageState.jsx
+++ usepopcorn/src/useLocaleStorageState.jsx
@@ -6,13 +6,13 @@ export function useLocalStorageState(initialState, key) {
     // DONT DO THIS: useState(localStorage.getItem('watched')) because this would get called on every render
     const storedValue = localStorage.getItem(key);
     return storedValue
-      ? JSON.parse(storedValue) // need the JSON.parse because we used stringify below to
+      ? JSON.parse(storedValue) // need the JSON.parse because we used stringify below, so its '[]' below, not just []
       : initialState;
   });
 
   useEffect(
     function () {
-      localStorage.setItem("watched", JSON.stringify(value));
+      localStorage.setItem("watched", JSON.stringify(value)); // You have to stringify because localStorage can only store strings
     },
     [value, key]
   );
