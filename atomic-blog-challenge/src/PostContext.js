ccd6b3dce9d7b8bbd0bdc0914b36b77fd8132e9e
Christian Yarwood <christianyarwood@gmail.com>
2025-11-04 11:10:26 +0000

    put value from provider context into useMemo

--- atomic-blog-challenge/src/PostContext.js
+++ atomic-blog-challenge/src/PostContext.js
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { useState, createContext } from "react";
 
 function createRandomPost() {
@@ -35,19 +35,17 @@ function PostProvider({ children }) {
     setPosts([]);
   }
 
-  return (
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
-  );
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchQuery, searchedPosts]);
+
+  return <PostContext.Provider value={value}>{children}</PostContext.Provider>;
 }
 
 function usePosts() {
