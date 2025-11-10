ea70c1f24e2cdde191cefe69d655ac59141bb5ed
Christian Yarwood <christianyarwood@gmail.com>
2025-11-10 20:03:07 +0000

    Formatting changes in code

--- travel-list-app/src/Form.js
+++ travel-list-app/src/Form.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 
 // React component for the form, onAddItems passed as props
-export default function Form({onAddItems}) {
-
+export default function Form({ onAddItems }) {
   // State management: sets the form initilisation no text and a quantity
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   // Function to handle the form submission
@@ -16,13 +15,13 @@ export default function Form({onAddItems}) {
     if (!description) return;
 
     // Create a new item using the state above
-    const newItem = {description, quantity, packed: false, id: Date.now()};
+    const newItem = { description, quantity, packed: false, id: Date.now() };
 
     // use the props declared above to run the function on the new item
     onAddItems(newItem);
 
     // Reset the form state inputs
-    setDescription('');
+    setDescription("");
     setQuantity(1);
   }
 
@@ -30,16 +29,24 @@ export default function Form({onAddItems}) {
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😁</h3>
       {/* This allows user to update state when selecting the quantity in the form */}
-      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-        {Array.from({length: 20}, (_, i) => i + 1)
-        .map(num => <option value={num} key={num}>
-          {num}
-        </option>)}
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
       {/* This updates the description state when you type something in */}
-      <input type='text' placeholder="Item..." value={description}
-      onChange={(e) => setDescription(e.target.value)}></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
       <button>Add</button>
     </form>
-  )
+  );
 }
